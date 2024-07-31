d34eaae9fe7c962d237e0e3ee9c83cf610283dcf
TakashiAihara <47384567+TakashiAihara@users.noreply.github.com>
2024-07-31 21:34:20 +0900

    migrate to vitest from jest (#30)
    * add vitest packages
    
    * modify vitest settings
    
    * modify integration tests
    
    * modify settings for vscode
    
    * add vitest globals type
    
    * remove jest packages, settings

---
+++ vitest.config.ts
@@ -0,0 +1,32 @@
+import swc from 'unplugin-swc';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    globals: true,
+    root: './',
+    watch: false,
+  },
+  plugins: [
+    swc.vite({
+      jsc: {
+        parser: {
+          syntax: 'typescript',
+          tsx: true,
+          dynamicImport: true,
+          decorators: true,
+        },
+        transform: {
+          legacyDecorator: true,
+          decoratorMetadata: true,
+          react: {
+            runtime: 'automatic',
+          },
+        },
+        target: 'esnext',
+        loose: false,
+      },
+      module: { type: 'es6' },
+    }),
+  ],
+});
